2ed72dbc046ea6a236b4c631f91cc0a6bd4da4ad
LAnselet <yichen.sam.sun@hotmail.com>
2024-04-10 00:14:54 -0700

    add node server user api

---
+++ src/models/user.js
@@ -0,0 +1,38 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  { timeStamps: true }
+);
+
+userSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+    // the passwordHash should not be revealed
+    // delete returnedObject.passwordHash;
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+export default User;
