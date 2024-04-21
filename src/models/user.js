ff9f0e3b2fb7c2f73636c01e5b647895ffb0088a
LAnselet <yichen.sam.sun@hotmail.com>
2024-04-21 14:04:43 -0700

    update node product, category, upload server for react app

--- src/models/user.js
+++ src/models/user.js
@@ -24,15 +24,5 @@ const userSchema = new mongoose.Schema(
   { timeStamps: true }
 );
 
-userSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
-    // the passwordHash should not be revealed
-    // delete returnedObject.passwordHash;
-  },
-});
-
 const User = mongoose.model('User', userSchema);
 export default User;
